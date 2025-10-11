a97055d20952152e81588e94b637394c16274236
Filip Leyko <fleyko@edu.cdv.pl>
2025-10-11 14:25:24 +0200

    lab2

---
+++ lab1/Lab2/Program.cs
@@ -0,0 +1,12 @@
+﻿//zadanie 4
+
+class Osoba
+{
+    public int Wiek;
+    public string Imie;
+    public void Przedstaw_sie()
+    {
+        Console.WriteLine($"Mam na imie: {Imie} i mam {Wiek}");
+    }
+}
+
